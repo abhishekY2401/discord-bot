2f7c20b0e589fc6b09056f046e189589926a7c8b
abhishekY2401 <abhiyd2401@gmail.com>
2021-11-26 17:54:51 +0530

    Added more features in the bot

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,9 @@
 import os
 import discord
+from discord import channel
 from discord.flags import Intents
+from discord.ext import commands, tasks
+import youtube_dl
 
 from dotenv import load_dotenv
 
@@ -33,6 +36,26 @@ async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
         f'Hello {member.name}!, Welcome to the {GUILD} 👋')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('hp!greet'):
+        channel = message.channel
+        await channel.send('Say Hello!')
+
+        def check(m):
+            return m.content == 'hello' and m.channel == channel
+
+        msg = await client.wait_for('message', check=check)
+        await channel.send(
+            'Hello {.author}! 👋\n'
+            'I am helping bot meant to help everyone. If you need any help then tag me with some of the following commands:\n'
+            '👉 hp!greet\n'
+            '👉 hp!qn\n'
+            '👉 hp!music\n'
+            'Sorry if there is a mistake, I am still under development 🙏'.
+            format(msg))
 
 
 client.run(TOKEN)
